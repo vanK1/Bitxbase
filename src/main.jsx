fc69a570d5d9b37463086eee6b35be174a424a50
VallEzeh <ezefrancis994@gmail.com>
2025-05-20 05:19:00 -0700

    vall again

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Book from "./Book";
 import Ui from "./Ui";
+import Vall from "./Vall";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Book />
     <Ui/> 
+    <Vall/>
   </StrictMode>
 );
