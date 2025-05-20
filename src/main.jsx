df46bc925c4f3a48817c49aa755d9ec31600d146
fapsz <fapszsamuelfapohunda@gmail.com>
2025-05-20 13:29:38 +0100

    fapsz

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Book from "./Book";
 import Ui from "./Ui";
+import Ayo from "./Ayo";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Book />
+    <Ayo/>
     <Ui/> 
   </StrictMode>
 );
